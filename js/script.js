93ae803078aff71c5725e7ee76a8d1fec786cf69
Issac Abraham <issaca32@gmail.com>
2021-09-01 11:52:52 +0100

    adss sticky navbar to main and mini navbar

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-console.log("started")
 
+// hamburger menu toggle
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.querySelectorAll('.navbar.mini ul')[0]
 
@@ -7,3 +7,19 @@ toggleButton.addEventListener('click', () =>{
     console.log("clicked")
     navbarLinks.classList.toggle('active');
 });
+
+// Sticky scroll
+const navbar = Array.from(document.querySelectorAll('.navbar')).filter(s =>
+    window.getComputedStyle(s).getPropertyValue('display') !== 'none'
+)[0];
+const navTop = navbar.offsetTop;
+function stickyNav() {
+    if(window.scrollY >= navTop){
+        document.body.classList.add("fixed-nav");
+        navbar.style.background("white");
+    }else{
+        document.body.classList.remove("fixed-nav");
+    }
+}
+
+window.addEventListener('scroll', stickyNav)
